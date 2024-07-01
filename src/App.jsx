29f44c2839c7fb88cd444344c29e9694d28f6d9c
JC Park <whdekf@naver.com>
2024-07-01 22:53:57 +0900

    CHORE : first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import './App.css'
+
+function App() {
+
+
+    return (
+        <>
+            <div className="bg-gray-800 w-screen h-12">
+            </div>
+            <div className="bg-gray-950 w-screen h-screen flex justify-center items-center text-white">
+
+            </div>
+        </>
+    )
+}
+
+export default App
