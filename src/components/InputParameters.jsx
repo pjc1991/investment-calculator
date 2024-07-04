9dcaf05875cb2380e51ee90713c2269cde8f6094
JC Park <whdekf@naver.com>
2024-07-04 23:03:03 +0900

    REFACTOR : 불필요한 아이디 삭제

--- src/components/InputParameters.jsx
+++ src/components/InputParameters.jsx
@@ -3,13 +3,13 @@ import BasicNumberInput from "./BasicNumberInput.jsx";
 export default function InputParameters({investmentParameters, handleEdit}) {
     return (
         <div className="grid grid-cols-8 gap-4 mx-auto mt-4 bg-green-800 p-4 rounded-lg w-1/2">
-            <BasicNumberInput label={"초기 투자 금액"} id={"initialInvestment"}
+            <BasicNumberInput label={"초기 투자 금액"}
                               initialValue={investmentParameters.initialInvestment} onEdit={handleEdit}/>
-            <BasicNumberInput label={"연간 투자 금액"} id={"yearlyInvestment"}
+            <BasicNumberInput label={"연간 투자 금액"}
                               initialValue={investmentParameters.yearlyInvestment} onEdit={handleEdit}/>
-            <BasicNumberInput label={"연 이자율"} id={"couponRate"} initialValue={investmentParameters.couponRate}
+            <BasicNumberInput label={"연 이자율"} initialValue={investmentParameters.couponRate}
                               onEdit={handleEdit}/>
-            <BasicNumberInput label={"투자 기간"} id={"years"} initialValue={investmentParameters.years}
+            <BasicNumberInput label={"투자 기간"} initialValue={investmentParameters.years}
                               onEdit={handleEdit}/>
         </div>
     )
