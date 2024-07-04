f0063e00051460cbad10d5b345787d92088b5a5c
JC Park <whdekf@naver.com>
2024-07-04 22:57:19 +0900

    REFACTOR : 직관적인 마크업으로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import {IconMoneybag} from "@tabler/icons-react";
 
 export default function Header() {
     return (
-        <>
+        <header>
             <div className="bg-gray-800 w-screen h-12">
             </div>
             <IconMoneybag className={"w-12 h-12 text-white mx-auto m-4"}/>
             <h1 className="text-3xl font-bold">
                 투자 계산기
             </h1>
-        </>
+        </header>
     )
 }
